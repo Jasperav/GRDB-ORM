dc3bffeb74c3e8686e5037ecb8ed0f678ed25830
Jasper <Jasperav@hotmail.com>
2021-08-07 16:19:37 +0200

    Improved

--- GRDBPerformance/GRDBPerformanceTests/Metadata.swift
+++ GRDBPerformance/GRDBPerformanceTests/Metadata.swift
@@ -1,13 +1,8 @@
 //
-// Created by Jasper Visser on 07/08/2021.
+//  Metadata.swift
+//  GRDBPerformanceTests
+//
+//  Created by Jasper Visser on 07/08/2021.
 //
 
 import Foundation
-import GRDBPerformance
-import XCTest
-
-class MetadataTest: XCTestCase {
-    func testMetadata() throws {
-        XCTAssertEqual(3, DbMetadata.tables().count)
-    }
-}
