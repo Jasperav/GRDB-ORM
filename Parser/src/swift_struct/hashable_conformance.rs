f1698f0f83c648fd9d9e045bb524c616a522b264
Jasper <Jasperav@hotmail.com>
2022-03-03 12:26:15 +0100

    rename

--- Parser/src/swift_struct/hashable_conformance.rs
+++ Parser/src/swift_struct/hashable_conformance.rs
@@ -4,12 +4,7 @@ pub fn write(tmd: &mut TableMetaData) {
     tmd.line_writer
         .add_line("public func hash(into hasher: inout Hasher) {");
 
-    for swift_property in tmd
-        .primary_keys()
-        .into_iter()
-        .cloned()
-        .collect::<Vec<_>>()
-    {
+    for swift_property in tmd.primary_keys().into_iter().cloned().collect::<Vec<_>>() {
         tmd.line_writer.add_line(format!(
             "hasher.combine({})",
             swift_property.swift_property_name
