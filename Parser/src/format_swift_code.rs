f7fa7868809ae985ddeabdb1ace1618ea537c0f3
jaspervisser <jasperav@hotmail.com>
2022-08-30 09:20:26 +0200

    Update version

--- Parser/src/format_swift_code.rs
+++ Parser/src/format_swift_code.rs
@@ -12,10 +12,16 @@ pub fn format_swift_code(config: &Config, safe_output_dir: &Path) {
             // TODO Not sure how the --swiftversion flag works, can't get it to work
             .args(&["."])
             .output()
-            .unwrap();
+            .expect(&format!(
+                "Problem formatting code in: {:#?}",
+                safe_output_dir
+            ));
 
         if !output.status.success() {
-            panic!("{}", String::from_utf8(output.stderr).unwrap())
+            panic!(
+                "Something went wrong: {}",
+                String::from_utf8(output.stderr).unwrap()
+            )
         }
     } else {
         println!("Not formatting Swift code due to configuration");
@@ -26,12 +32,16 @@ pub fn format_swift_code(config: &Config, safe_output_dir: &Path) {
 
         let output = Command::new("swiftlint")
             .current_dir(safe_output_dir)
-            .args(&["autocorrect"])
+            .args(&["--fix"])
             .output()
             .unwrap();
 
         if !output.status.success() {
-            panic!("{}", String::from_utf8(output.stderr).unwrap())
+            panic!(
+                "Something went wrong while using Swiftlint at path '{:#?}': {}",
+                safe_output_dir,
+                String::from_utf8(output.stderr).unwrap()
+            )
         }
     } else {
         println!("Not autocorrecting Swift code due to configuration");
