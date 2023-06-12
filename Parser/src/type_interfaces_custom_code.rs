f46ae8402516525b7adb6395177baf4a3724d689
jaspervisser <jasperav@hotmail.com>
2023-06-12 20:13:42 +0200

    Update version

--- Parser/src/type_interfaces_custom_code.rs
+++ Parser/src/type_interfaces_custom_code.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use grdb_orm_lib::room::TypeInterfacesCustomCode;
 use grdb_orm_lib::serde::Deserialize;
 use grdb_orm_lib::toml::{Deserializer, Value};
@@ -8,10 +7,13 @@ read!(Vec<TypeInterfacesCustomCode>);
 fn transform(content: &str) -> Vec<TypeInterfacesCustomCode> {
     let value: Value = content.parse().unwrap();
     let tables = value.as_table().unwrap();
-    let mut types = vec![];
+    let mut types: Vec<TypeInterfacesCustomCode> = vec![];
 
     for (_, value) in tables {
-        let deserialized = TypeInterfacesCustomCode::deserialize(value).unwrap();
+        let deserialized = TypeInterfacesCustomCode::deserialize(
+            grdb_orm_lib::toml::de::ValueDeserializer::new(&value.to_string()),
+        )
+        .unwrap();
 
         assert!(!types.iter().any(|t| t.ty == deserialized.ty));
 
