b055ecc81e8b26ae12781baa6a49521872dc0701
jaspervisser <jasperav@hotmail.com>
2023-06-12 20:09:14 +0200

    Update version

--- Parser/src/type_interfaces_custom_code.rs
+++ Parser/src/type_interfaces_custom_code.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use grdb_orm_lib::room::TypeInterfacesCustomCode;
 use grdb_orm_lib::serde::Deserialize;
 use grdb_orm_lib::toml::{Deserializer, Value};
@@ -5,5 +6,10 @@ use grdb_orm_lib::toml::{Deserializer, Value};
 read!(Vec<TypeInterfacesCustomCode>);
 
 fn transform(content: &str) -> Vec<TypeInterfacesCustomCode> {
-    grdb_orm_lib::toml::from_str(content).unwrap()
+    let deserialize: Vec<TypeInterfacesCustomCode> = grdb_orm_lib::toml::from_str(content).unwrap();
+    let uniques = deserialize.iter().map(|d| &d.ty).collect::<HashSet<_>>();
+
+    assert_eq!(uniques.len(), deserialize.len());
+
+    deserialize
 }
