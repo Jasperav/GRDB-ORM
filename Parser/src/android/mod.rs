6b67eb275bba441e460b09ebe3489d5cec669a02
jaspervisser <jasperav@hotmail.com>
2023-10-11 10:13:53 +0200

    Suppress warnings for Android Room

--- Parser/src/android/mod.rs
+++ Parser/src/android/mod.rs
@@ -4,7 +4,7 @@ pub use kotlin::*;
 use std::path::Path;
 mod entities;
 
-static SUPPRESS_ALL: &str = "@file:Suppress(\"ALL\")";
+static SUPPRESS_ALL: &str = "@file:Suppress(\"warnings\")";
 
 pub fn generate_kotlin_package(path: &Path) -> String {
     let mut package_parts = vec![];
